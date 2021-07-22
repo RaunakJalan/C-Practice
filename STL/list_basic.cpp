3d18f7b3ada1f5813f8a4730ce83392c96903a73
RaunakJalan <ronakjalan98@gmail.com>
2021-07-23 04:30:24 +0530

    More Advanced List Functions

--- STL/list_basic.cpp
+++ STL/list_basic.cpp
@@ -67,6 +67,50 @@ int main(){
         cout<<s<<"-->";
     }
     cout<<endl;
+
+    // Some more functions in list
+    l2.push_back("orange");
+    l2.push_back("lemon");
+    l2.push_back("guava");
+    for(string s:l2){
+        cout<<s<<"-->";
+    }
+    cout<<endl;
+
+    // Remove a fruit by name
+    string f;
+    cout<<"Enter fruit name to remove:";
+    cin>>f;
+    l2.remove(f);
+
+    for(string s:l2){
+        cout<<s<<"-->";
+    }
+    cout<<endl;
+
+    // Erase funtion to rmeove one or more element using index
+    //l2.begin()+3 does not work for list because it is not a linear memory
+    auto it = l2.begin();
+    it++;
+    it++;
+    l2.erase(it);
+    for(string s:l2){
+        cout<<s<<"-->";
+    }
+    cout<<endl;
+
+    // Insert elements in list
+    it = l2.begin();
+    it++;
+    l2.insert(it, "FruitJuice");
+
+    for(string s:l2){
+        cout<<s<<"-->";
+    }
+    cout<<endl;
+
+
+
     
     return 0;
 }
